e802b44b8a13a7391fe507f7d349103ba32713ff
dondrzzy <sibo.donald16@gmail.com>
2018-04-24 17:51:34 +0300

    ft #156577835 Fix front tests

--- hc/settings.py
+++ hc/settings.py
@@ -96,6 +96,8 @@ if os.environ.get("DB") == "postgres":
             'ENGINE':   'django.db.backends.postgresql',
             'NAME':     'hc',
             'USER':     'postgres',
+            'PASSWORD': 'engneerdon',
+            'HOST': 'localhost',
             'TEST': {'CHARSET': 'UTF8'}
         }
     }
