5db331ba70a18208d0f635df330fd28c6fa26e3d
dondrzzy <sibo.donald16@gmail.com>
2018-04-24 17:51:34 +0300

    ft #156577835 Fix front tests

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -14,7 +14,9 @@ class AddCheckTestCase(BaseTestCase):
     ### Test that team access works
     def test_that_team_access_works(self):
         url = "/checks/add/"
+
         self.client.login(username="bob@example.org", password="password")
+
         r = self.client.post(url)
         self.assertRedirects(r, "/checks/")
         assert Check.objects.count() == 1
